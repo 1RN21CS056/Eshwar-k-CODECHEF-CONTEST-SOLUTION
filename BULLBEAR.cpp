88b77aa30e2fa06ffc66f8f2a627aa5967d31116
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2023-02-16 19:30:48 +0530

    Add files via upload

---
+++ BULLBEAR.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	   int X,Y;
+	   cin>>X>>Y;
+	   if(X<Y){
+	       cout<<"PROFIT"<<endl;
+	   }
+	   else if(X>Y){
+	       cout<<"LOSS"<<endl;
+	   }
+	   else{
+	       cout<<"NEUTRAL"<<endl;
+	   }
+	}
+	return 0;
+}
